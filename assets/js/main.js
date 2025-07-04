b6f161493abb6b3dabc998fc55e2d06bf27fdfe4
CmloPvl <camiloprg2@gmail.com>
2025-07-03 22:50:54 -0400

    jugangoconelportafolio

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const sections = document.querySelectorAll('section');
+
+  function checkVisibility() {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    sections.forEach(section => {
+      const sectionTop = section.getBoundingClientRect().top;
+
+      if (sectionTop < triggerBottom) {
+        section.classList.add('visible');
+      }
+    });
+  }
+
+  window.addEventListener('scroll', checkVisibility);
+  checkVisibility();
+});
